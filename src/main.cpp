2558b5124365ce6e492882f28bf3a774785a06fd
ZRE <chy853@gmail.com>
2024-04-09 00:30:25 +0800

    Revert "Update FPS_CAP"
    This reverts commit b1c7858ef4d5e896cdeefb9ff83c759df451e5e0.

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,25 @@
 
 int main(int, char **) {
     auto context = Core::Context::GetInstance();
-    App app;
-    constexpr unsigned int MAIN_FPS_CAP = 60;
-    constexpr double frameTime =
-        MAIN_FPS_CAP != 0 ? 1000 / static_cast<double>(MAIN_FPS_CAP) : 0;
-
-    auto a = Util::Time::GetElapsedTimeMs(), b = Util::Time::GetElapsedTimeMs(),
-         delta = a - b;
+    App  app;
 
     while (!context->GetExit()) {
-        a = Util::Time::GetElapsedTimeMs();
-        delta = a - b;
-        if (delta > frameTime) {
-            context->Setup();
+        context->Setup();
 
-            switch (app.GetCurrentState()) {
-            case App::State::START:
-                app.Start(context);
-                break;
+        switch (app.GetCurrentState()) {
+            case App::State::START: app.Start(context); break;
 
-            case App::State::UPDATE:
-                app.Update();
-                break;
+            case App::State::UPDATE: app.Update(); break;
 
             case App::State::END:
                 app.End();
                 context->SetExit(true);
                 break;
-            }
-
-            b = a;
-            ImGui::Render();
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-            context->Update();
         }
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        context->Update();
     }
     return 0;
 }
